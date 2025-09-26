f836fbc9459b3b0928f9305b2fc23af131026542
mpalle4 <141689250+mpalle4@users.noreply.github.com>
2025-09-25 21:05:57 -0300

    Update test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,1 +1,13 @@
-<file-upload>
+# tests/test_utils.py
+from utils import calculate_top_sellers, summarize_sales
+
+def test_calculate_top_sellers_basic():
+    sales = [("a", 2), ("b", 3), ("a", 1)]
+    res = calculate_top_sellers(sales, top_n=2)
+    assert res[0][0] == "b" or res[0][0] == "a"  # orden según cantidades
+
+def test_summarize_sales_empty():
+    res = summarize_sales([])
+    assert res["total_items"] == 0
+    assert res["unique_products"] == 0
+    assert res["average_per_product"] == 0
